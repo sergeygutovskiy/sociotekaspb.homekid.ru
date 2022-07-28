0b8d902650afe72ead243a788b09cca37ba0a030
Сергей Гутовский <sergey.gutovsk@gmail.com>
2022-07-28 11:36:24 +0300

    removed old code, that going to be recreate

--- routes/client/v1/api.php
+++ routes/client/v1/api.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\Client\AuthController;
 use App\Http\Controllers\Client\CompanyController;
 use App\Http\Controllers\Client\DictionaryCategoryController;
-use App\Http\Controllers\Client\FileController;
-use App\Http\Controllers\Client\Job\Implementation\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/users')->group(function() {
@@ -15,17 +13,8 @@ Route::prefix('/users')->group(function() {
 Route::middleware(['auth:sanctum'])->group(function() {
     Route::get('/dictionary-categories/{category}/dictionaries', [ DictionaryCategoryController::class, 'dictionaries' ]);
 
-    Route::post('/files', [ FileController::class, 'store' ]);
-
     Route::prefix('/company')->group(function() {
         Route::get('/', [ CompanyController::class, 'show' ]);
         Route::put('/', [ CompanyController::class, 'update' ]);
-    });
-
-    Route::prefix('/jobs')->group(function() {
-        Route::prefix('/projects')->group(function() {
-            Route::get('/{id}', [ ProjectController::class, 'show' ]);            
-            Route::post('/', [ ProjectController::class, 'store' ]);
-        });
     });
 });
