d2acf06b2efc9d9365fb9d4da885b86a51e398f9
gutovskiy.ru <sergey.gutovsk@gmail.com>
2022-07-30 19:07:54 +0300

    added createting of social project

--- app/Http/Controllers/Client/Jobs/SocialProjectController.php
+++ app/Http/Controllers/Client/Jobs/SocialProjectController.php
@@ -5,11 +5,43 @@ namespace App\Http\Controllers\Client\Jobs;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Client\Job\SocialProject\StoreRequest;
 use App\Http\Responses\OKResponse;
+use App\Models\Job\Job;
+use App\Models\Job\JobContacts;
+use App\Models\Job\JobExperience;
+use App\Models\Job\JobPrimaryInformation;
+use App\Models\Job\JobReportingPeriod;
 
 class SocialProjectController extends Controller
 {
     public function store(StoreRequest $request)
     {
-        return OKResponse::response();
+        $validated_data = $request->validated();
+
+        $primary_data = $validated_data['primary'];
+        $contacts_data = $validated_data['contacts'];
+        $experience_data = $validated_data['experience'];
+        $info_data = $validated_data['info'];
+        $reporting_periods_data = $validated_data['reporting_periods'];
+
+        $job_primary = JobPrimaryInformation::create($primary_data);
+        $job_contacts = JobContacts::create($contacts_data);
+        $job_experience = JobExperience::create($experience_data);
+
+        $job = Job::create([
+            'primary_information_id' => $job_primary->id,
+            'experience_id' => $job_experience->id,
+            'contacts_id' => $job_contacts->id,
+        ]);
+
+        $reporting_periods = array_map(fn($data) => new JobReportingPeriod($data), $reporting_periods_data);
+        $job->reporting_periods()->saveMany($reporting_periods);
+
+        $social_project = $job->social_projects()->create($info_data);
+
+        return OKResponse::response([
+            'social_project' => [
+                'id' => $social_project->id 
+            ],
+        ]);
     }
 }
--- app/Models/Job/Job.php
+++ app/Models/Job/Job.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'primary_information_id',
+        'experience_id',
+        'contacts_id',
+    ];
+
+    public function reporting_periods()
+    {
+        return $this->hasMany(JobReportingPeriod::class);
+    }
+
+    public function social_projects()
+    {
+        return $this->hasMany(SocialProject::class);
+    }
 }
