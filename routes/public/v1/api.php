544fb0f451beb68cde1fbe921a9aebd635e8e159
gutovskiy.ru <sergey.gutovsk@gmail.com>
2022-08-28 21:46:27 +0300

    fixed public dictionary endpoint

--- routes/public/v1/api.php
+++ routes/public/v1/api.php
@@ -14,10 +14,9 @@ Route::prefix('/dictionaries')->group(function() {
     Route::prefix('/categories')->group(function() {
         Route::get('/rnsu-category/groups', [ RnsuCategoryGroupController::class, 'index' ]);
 
-        Route::get('/{category}', [ DicionaryController::class, 'dictionaries' ]);
         Route::get('/{parent_category_slug}/{child_category_slug}', [ DictionaryCategoryController::class, 'child_dictionaries' ]);
+        Route::get('/{category}', [ DictionaryCategoryController::class, 'dictionaries' ]);
     });
-
     Route::get('/jobs/reporting-periods/years', [ DicionaryController::class, 'job_reporting_period_years' ]);
 });
 
