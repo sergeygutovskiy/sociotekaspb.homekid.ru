d1725887160e95c40d81842d5b0af1ab1e1dc4fb
gutovskiy.ru <sergey.gutovsk@gmail.com>
2022-08-30 15:44:00 +0300

    added id for rnsu group

--- app/Http/Resources/Public/RnsuCategoryGroup/Resource.php
+++ app/Http/Resources/Public/RnsuCategoryGroup/Resource.php
@@ -9,6 +9,7 @@ class Resource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'label' => $this->label,
             'image_path' => $this->image_path,
             'rnsu_ids' => $this->rnsu_ids,
