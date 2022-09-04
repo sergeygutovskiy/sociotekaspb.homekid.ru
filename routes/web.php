c0bf0f6c3e3a0ad3e248fc2df2bd3774961a7195
gutovskiy.ru <sergey.gutovsk@gmail.com>
2022-09-04 17:23:18 +0300

    updated web routes

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn() => view('pages.landing.app'));
+Route::get('/{any}', fn() => view('pages.landing.app'))->where('any', '.*');
 Route::get('/account{any}', fn() => view('pages.account.app'))->where('any', '.*');
