d170d2336addeddadcc7ce44abe0ca989a8041fe
gutovskiy.ru <sergey.gutovsk@gmail.com>
2023-03-26 04:30:20 +0300

    testing

--- resources/views/pages/landing/app.blade.php
+++ resources/views/pages/landing/app.blade.php
@@ -15,6 +15,23 @@
 
     <title> Социотека СПб </title>
     <meta title="description" content="Единая база методик и технологий, разработанных и применяемых организациями социального обслуживания населения Санкт-Петербурга при предоставлении социальных услуг">
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+        ym(91664933, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/91664933" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
     <div id="root"></div>
