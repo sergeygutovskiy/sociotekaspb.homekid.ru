74165145a1bfeafb4cd9e037d9b17ac0cac7bdd1
gutovskiy.ru <sergey.gutovsk@gmail.com>
2023-03-26 20:48:40 +0300

    testing

--- app/Http/Services/Admin/CompanyService.php
+++ app/Http/Services/Admin/CompanyService.php
@@ -16,10 +16,27 @@ class CompanyService
         $status_filter = $request->input('filter_status');
         $district_filter = $request->input('filter_district_id');
 
+        $is_education_license_filter = $request->input('filter_is_education_license');
+        $is_medical_license_filter = $request->input('filter_is_medical_license');
+        $is_has_innovative_platform_filter = $request->input('filter_is_has_innovative_platform');
+
         $query = Company::query()->notTest();
         if ( $name_filter ) $query = $query->where('name', 'like', '%'.$name_filter.'%');
         if ( $status_filter ) $query = $query->where('status', $status_filter);
         if ( is_numeric($district_filter) ) $query = $query->where('district_id', $district_filter);
+
+        if ( !is_null($is_education_license_filter) ) 
+        {
+            $query = $is_education_license_filter ? $query->whereNot('education_license', null) : $query->where('education_license', null);
+        }
+        if ( !is_null($is_medical_license_filter) ) 
+        {
+            $query = $is_medical_license_filter ? $query->whereNot('medical_license', null) : $query->where('medical_license', null);
+        }
+        if ( !is_null($is_has_innovative_platform_filter) ) 
+        {
+            $query = $query->where('is_has_innovative_platform', !!$is_has_innovative_platform_filter);
+        }
 
         $sort_by = $request->input('sort_by');
         $sort_direction = $request->input('sort_direction');
